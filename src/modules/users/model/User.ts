964f417feb1d91f04c374f28c93dc62720977f1c
mozxf <nandogustavo2001*gmail.com>
2022-05-14 23:02:34 -0300

    Documented API using swagger-ui-express

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -9,15 +9,14 @@ class User {
   updated_at: Date;
 
   constructor(name: string, email: string) {
+    if (!this.id) {
+      this.id = uuidv4();
+    }
     this.name = name;
     this.email = email;
     this.admin = false;
     this.created_at = new Date();
     this.updated_at = new Date();
-
-    if (!this.id) {
-      this.id = uuidv4();
-    }
   }
 }
 
